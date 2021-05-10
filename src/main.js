e87775285a0b8eea476cfafe415f2abb92f18c91
Marcos Vinicius Rego Freire <mvinnicius22@hotmail.com>
2021-05-10 01:36:53 -0300

    10/05 01:36 data-table + moment

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
+import moment from 'moment'
+import VueMoment from 'vue-moment'
+require('moment/locale/pt-br')
 
 Vue.config.productionTip = false
+Vue.use(VueMoment, {
+  moment
+})
 
 new Vue({
   router,
